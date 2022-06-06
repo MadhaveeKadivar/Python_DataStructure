832de199ec02955456cb32a178c6e35a007beb80
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-06-06 18:12:00 +0530

    [Madhavee] Add : Program to reverse string

---
+++ String/11_Reverse_string.py
@@ -0,0 +1,11 @@
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-18 10:50:04
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-18 10:50:04
+    @Title : Reverse string
+'''
+# Main Code
+if __name__=="__main__":
+    string = input("\nEnter any string: ")
+    print(f"\n\"{string}\" in reverse order : {string[::-1]}")
