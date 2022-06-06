b2e574f78f18743f9bc7e1154333a435fc491ae1
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-06-06 19:01:40 +0530

    [Madhavee] Add : Program to create tuple

---
+++ Tuple/1_Create_tuple.py
@@ -0,0 +1,13 @@
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-18 18:05:08
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-18 18:05:08
+    @Title : Create tuple
+'''
+if __name__=="__main__":
+    empty_ele = ()
+    print(f"\nEmpty tuple is : {empty_ele}")
+    tuple_ele = (1,2,3,4,1)
+    print(f"\nTuple with element : {tuple_ele}")
+    
