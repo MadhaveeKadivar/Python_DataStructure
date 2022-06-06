a6e9d794e75eea4178bf020127850adb09b686b0
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-06-06 18:53:49 +0530

    [Madhavee] Add : Program to create set

---
+++ Set/1_Create_set.py
@@ -0,0 +1,11 @@
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-18 14:30:53
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-18 14:30:53
+    @Title : Create Set
+'''
+if __name__=="__main__":
+    new_set = {5,6,6,"Hi",'M'} # Creating set
+    print(f"Set : {new_set}")
+        
