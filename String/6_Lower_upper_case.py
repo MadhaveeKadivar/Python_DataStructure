c3630d9a2515adb26bf03facb78cade6edd75467
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-06-06 18:10:17 +0530

    [Madhavee] Add : Program to get uppercase and lowercase of string

---
+++ String/6_Lower_upper_case.py
@@ -0,0 +1,12 @@
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-17 22:54:00
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-17 22:54:00
+    @Title : Lower and Upper case
+'''
+# Main Code
+if __name__=="__main__":
+    string = input("\nEnter any string : ")
+    print(f"\nString in upper case : {string.upper()}")
+    print(f"\nString in lower case : {string.lower()}")
