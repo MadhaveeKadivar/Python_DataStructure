a78cc1b8f25fba92ae1ef0e85be9286a2c6a8761
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-06-06 18:08:12 +0530

    [Madhavee] Add : Program to get string length

---
+++ String/1_Length_of_string.py
@@ -0,0 +1,10 @@
+'''
+    @Author: Madhavee Kadivar
+    @Date: 2022-05-17 22:03:25
+    @Last Modified by: Madhavee Kadivar
+    @Last Modified time: 2022-05-17 22:03:25
+    @Title : Length of string
+'''
+if __name__=="__main__":
+    string = input("\nEnter any string : ")
+    print(f"\nLength of \"{string}\" is : {len(string)}\n")
